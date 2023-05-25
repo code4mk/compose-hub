c7ed51b6a4dad3098a3eb84b2a58c6dc5f55e325
Mostafa Kamal <hiremostafa@gmail.com>
2023-05-25 15:08:07 +0600

    environment set

--- db/mysql-phpmyadmin/start_db.sh
+++ db/mysql-phpmyadmin/start_db.sh
@@ -2,10 +2,10 @@
 curl -o db_compose.yml -LJO https://raw.githubusercontent.com/code4mk/compose-hub/main/db/mysql-phpmyadmin/compose.yml
 
 # compose up
+MYSQL_VESRION=8 \
+MYSQL_PORT=3336 \
+PHP_MYADMIN_VERSION=4.9 \
+PHP_MYADMIN_PORT=8010 \
 docker compose \
   --file db_compose.yml \
-  up -d \
-  -e MYSQL_VESRION=8 \
-  -e MYSQL_PORT=3336 \
-  -e PHP_MYADMIN_VERSION=4.9 \
-  -e PHP_MYADMIN_PORT=8010
+  up -d 
