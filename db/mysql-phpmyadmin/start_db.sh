aa3df7acbdef73fb32265b84cb6e94b6fbe06afa
Mostafa Kamal <hiremostafa@gmail.com>
2023-05-25 13:25:17 +0600

    environment setup

--- db/mysql-phpmyadmin/start_db.sh
+++ db/mysql-phpmyadmin/start_db.sh
@@ -4,4 +4,8 @@ curl -o db_compose.yml -LJO https://raw.githubusercontent.com/code4mk/compose-hu
 # compose up
 docker compose \
   --file db_compose.yml \
-  up -d
+  up -d \
+  -e MYSQL_VESRION=8 \
+  -e MYSQL_PORT=3336 \
+  -e PHP_MYADMIN_VERSION=4.9 \
+  -e PHP_MYADMIN_PORT=8010
