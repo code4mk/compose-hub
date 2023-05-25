7108a15f78e5d9c6c8410a5628aad7b8b1c8aac7
Mostafa Kamal <hiremostafa@gmail.com>
2023-05-25 17:23:08 +0600

    bash

--- db/mysql-phpmyadmin/start_db.sh
+++ db/mysql-phpmyadmin/start_db.sh
@@ -1,11 +1,19 @@
-# https://github.com/code4mk/compose-hub/tree/main/db/mysql-phpmyadmin
-curl -o db_compose.yml -LJO https://raw.githubusercontent.com/code4mk/compose-hub/main/db/mysql-phpmyadmin/compose.yml
+read -p "Database compose up/down? " answer
 
-# compose up
-MYSQL_VESRION=8 \
-MYSQL_PORT=3336 \
-PHP_MYADMIN_VERSION=4.9 \
-PHP_MYADMIN_PORT=8010 \
-docker compose \
-  --file db_compose.yml \
-  up -d 
+if [[ $answer = "up" ]]; then
+  # https://github.com/code4mk/compose-hub/tree/main/db/mysql-phpmyadmin
+  curl -o db_compose.yml -LJO https://raw.githubusercontent.com/code4mk/compose-hub/main/db/mysql-phpmyadmin/compose.yml
+
+  # compose up
+  MYSQL_VESRION=8 \
+  MYSQL_PORT=3336 \
+  PHP_MYADMIN_VERSION=4.9 \
+  PHP_MYADMIN_PORT=8010 \
+  docker compose \
+    --file db_compose.yml \
+    up -d
+elif [[ $answer = "down" ]]; then
+  docker compose \
+    --file db_compose.yml \
+    down
+fi
